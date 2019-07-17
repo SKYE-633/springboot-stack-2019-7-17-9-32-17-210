b1f6f80e5d7e8cdb63b52c32f8e9daaba0d1e3dc
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-17 21:02:16 +0800

    Add files via upload
    Story1 AC1---AC5

--- src/main/java/com/tw/apistackbase/controller/HelloResource.java
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -1,27 +1,54 @@
 package com.tw.apistackbase.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
  * Created by jxzhong on 18/08/2017.
  */
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/criminalCases")
 public class HelloResource {
 
     private final Logger log = Logger.getLogger(this.getClass().getName());
 
-    @GetMapping(path = "/{userName}", produces = {"application/json"})
-    public ResponseEntity<String> getAll(@PathVariable String userName) {
+//    @GetMapping(path = "/{userName}", produces = {"application/json"})
+//    public ResponseEntity<String> getAll(@PathVariable String userName) {
+//
+//        return ResponseEntity.ok("Hello:" + userName);
+//    }
+    private List<CriminalCase> result = new ArrayList();
+    @GetMapping()
 
-        return ResponseEntity.ok("Hello:" + userName);
+    public ResponseEntity getAllCriminalCase(){
+
+        result.add(new CriminalCase());
+        return ResponseEntity.ok().body(result);
     }
+
+    @PostMapping()
+    public ResponseEntity createCriminalCase(@RequestBody CriminalCase criminalCase){
+        result.add(criminalCase);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity putCriminalCase(@PathVariable Integer id,@RequestBody CriminalCase criminalCase){
+        result.add(criminalCase);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteCriminalCase(@PathVariable Integer id,@RequestBody CriminalCase criminalCase){
+        result.remove(criminalCase);
+        return ResponseEntity.ok().build();
+    }
+
 
 
 }
