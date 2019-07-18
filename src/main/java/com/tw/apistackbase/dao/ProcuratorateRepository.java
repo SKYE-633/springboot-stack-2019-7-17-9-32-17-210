52684c12004739ea33c5078b58c6bcb5c2640b1d
homework <52529600+SKYE-633@users.noreply.github.com>
2019-07-18 18:37:30 +0800

    Delete ProcuratorateRepository.java

--- src/main/java/com/tw/apistackbase/dao/ProcuratorateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tw.apistackbase.dao;
-
-import com.tw.apistackbase.entity.Procuratorate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProcuratorateRepository extends JpaRepository<Procuratorate, Integer> {
-}
